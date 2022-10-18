e964c098e3eba59039aeebeba690a9b92488b115
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-10-18 17:43:43 +0300

    Added redux and components

---
+++ app/javascript/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import store from "../redux/store";
+import Greetings from "./Greetings";
+
+const App = () => {
+  return (
+    <>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Greetings />} />
+          </Routes>
+        </Router>
+      </Provider>
+    </>
+  );
+};
+
+export default App;
