c1f8651e77269bc58f1a7f8dcf4d521cad4de808
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-10-18 17:45:24 +0300

    Corrected rubocop errors

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MessagesController < ApplicationController
-    def index
-        message = Message.all.sample.greeting
-        # render json: messages
-        render json: {status: 'SUCCESS', message: 'Messages Loaded', greeting: message }, status: :ok 
-    end
+  def index
+    message = Message.all.sample.greeting
+    # render json: messages
+    render json: { status: 'SUCCESS', message: 'Messages Loaded', greeting: message }, status: :ok
+  end
 end
